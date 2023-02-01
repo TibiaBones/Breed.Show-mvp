6feb7a354d11a9ee2632b1bd4c168cb71d6b656c
TibiaBones <78929148+TibiaBones@users.noreply.github.com>
2023-02-01 04:13:31 +0300

    add correct status block toggle

--- loginRegistrationScript.js
+++ loginRegistrationScript.js
@@ -1,40 +1,24 @@
 let statusIndex = 1;
 const breedBlockId = document.getElementById("breederContainer");
-// let buttonsYes = [document.getElementById("buttonOn1"), document.getElementById("buttonOff1")];
-// let buttonsNo = [document.getElementById("buttonOn2"), document.getElementById("buttonOff2")];
-console.log(statusIndex);
+const buttons = document.querySelectorAll(".radio-statusСhoiceButton_img");
 
 function breederStatus(){
     if(statusIndex == 2){
         statusIndex = 1;
         breedBlockId.style.display = "flex";
-
-        // for(let but of buttonsYes){
-        //     if()
-        // }
-        document.getElementById("buttonOn1").style.display = "block";
-        document.getElementById("buttonOff1").style.display = "none";
-    }else{
-        document.getElementById("buttonOn1").style.display = "none";
-        document.getElementById("buttonOff1").style.display = "block";
+        buttons.forEach(i => i.classList.toggle('off'))
     };
-    console.log(statusIndex);
 };
 
 function userStatus (){
     if(statusIndex == 1){
         statusIndex = 2;
         breedBlockId.style.display = "none";
-        document.getElementById("buttonOn2").style.display = "block";
-        document.getElementById("buttonOff2").style.display = "none";
-    }else{
-        document.getElementById("buttonOn2").style.display = "none";
-        document.getElementById("buttonOff2").style.display = "block";
+        buttons.forEach(i => i.classList.toggle('off'))
     };
-
-    console.log(statusIndex);
 };
 
+// переписать, объединить функции в одну, применить addEventListener вместо oncklick
 
 
 
